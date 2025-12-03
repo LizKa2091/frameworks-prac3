eda391656039f1fe75af2659c0040bebd0137df6
lizka <lizka4231@gmail.com>
2025-12-03 23:59:22 +0300

    feat: add css visualization

--- services/php-web/laravel-patches/resources/views/layouts/app.blade.php
+++ services/php-web/laravel-patches/resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
   <link rel='stylesheet' href='/css/animations.css'>
+  <link rel="stylesheet" href="/css/ui.css">
   <style>#map{height:340px}</style>
   <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
